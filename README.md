2895448bad873b1e5c11f865b2879642ca7ae9b0
joshuabrandes <47532061+joshuabrandes@users.noreply.github.com>
2024-12-26 22:53:05 +0100

    adds REST controller and application config

---
+++ README.md
@@ -0,0 +1,3 @@
+# httpstat.us test-app (imperative)
+
+This benchmark is inspired by a presentation of Josh Long from Broadcom at DATEV.
---
+++ src/main/java/net/joshuabrandes/httpstatus_test/EndpointController.java
@@ -0,0 +1,20 @@
+package net.joshuabrandes.httpstatus_test;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClient;
+
+@RestController
+@RequestMapping("/v1")
+public class EndpointController {
+
+    @GetMapping("/status")
+    public ResponseEntity<String> status() {
+        return RestClient.create().get()
+                .uri("https://httpstat.us/200?sleep=5000")
+                .retrieve()
+                .toEntity(String.class);
+    }
+}
